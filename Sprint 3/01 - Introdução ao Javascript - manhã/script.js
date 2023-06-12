696a2e440953d14bf719e05facd4f756992b9291
vilsonpneto <vilson@kenzie.com.br>
2023-06-12 12:26:09 -0300

    Add files

---
+++ Sprint 3/01 - Introdução ao Javascript - manhã/script.js
@@ -0,0 +1,63 @@
+// console.log("Olá turma!");
+// console.log("Vilson");
+// console.log("Thiago");
+// alert("Olá pessoal");
+
+// let -> utilizamos quando futuramente precisaremos alterar o valor
+let nome = "Vilson";
+
+// const -> utilizamos quando não alteraremos o valor passado
+const idade = 24;
+
+// Não utilizamos var
+// var email = "golimar@kenzie.com";
+
+// console.log(nome);
+// console.log(idade);
+
+// idade = 25;
+// console.log(idade);
+
+nome = "Vilson Padilha Neto";
+
+{
+  nome = "Batata";
+  const usuario = "Golimar";
+  //   console.log(usuario);
+}
+// console.log(usuario);
+
+// console.log(nome);
+
+// STRING
+let texto = "Olá pessoal, meu username é 'Golimar'!";
+// let texto2 = 'Batatinha';
+let texto3 = `Golimar`;
+console.log(typeof texto3);
+
+// NUMBER
+let numeroInteiro = 154654564654654654;
+let numeroDecimal = 0.55555555555;
+let numeroNegativo = -123456;
+console.log(typeof numeroDecimal);
+
+// BOOLEAN -> Boleano
+// true -> verdadeiro
+let boleano1 = true;
+// false -> falso
+let boleano2 = false;
+console.log(typeof boleano1);
+
+const a = 5;
+const b = 10;
+
+console.log(a + b);
+
+let teste = "Batatinha";
+teste = 10;
+teste = true;
+console.log(teste);
+
+const produto = "Batata";
+const preco = 3;
+const enreco = "Rua x, 455";
