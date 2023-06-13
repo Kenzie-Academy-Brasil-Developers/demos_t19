fc237aab4c158c5e78d505cbbd392020644c4c25
thiagoKenzie <102174562+thiagoKenzie@users.noreply.github.com>
2023-06-13 10:02:04 -0300

    Add files via upload

---
+++ Sprint 3/01 - Introdução ao Javascript - noite/script.js
@@ -0,0 +1,45 @@
+console.log('Kenzie Academy Espanha');
+console.log('Kenzie Academy Brasil');
+
+/*alert('Aviso para o usuário');*/
+
+// Variáveis
+
+// let - palavra reservada para declaração da variavel
+// teste - nome da variavel
+// = 1 - valor inicial da variavel
+
+let teste = 22;
+console.log(teste);
+teste = 15;
+console.log(teste);
+
+const teste2 = 55;
+console.log(teste2);
+// teste2 = 66;
+// console.log(teste2);
+
+var teste3 = 44;
+console.log(teste3);
+teste3 = 79;
+console.log(teste3);
+
+
+const numero = 3;               //number
+const numeroFloat = 3.1415;     //number
+const texto = 'thiago kenzie';  //string
+const boleano = true;           //boolean
+
+// console.log(typeof numero);
+// console.log(typeof numeroFloat);
+// console.log(typeof texto);
+// console.log(typeof boleano);
+
+
+let a = 12;
+let b = 3;
+let resultado = a/b;
+
+console.log(resultado);
+
+console.log(a,'/',b,'=',resultado);
