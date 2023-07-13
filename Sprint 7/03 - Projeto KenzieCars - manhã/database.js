e0f4cb64e62d7a978aa6c5861127cdbcfd5f2da5
thiagoKenzie <102174562+thiagoKenzie@users.noreply.github.com>
2023-07-13 10:56:01 -0300

    Add files via upload

---
+++ Sprint 7/03 - Projeto KenzieCars - manhã/database.js
@@ -0,0 +1,51 @@
+const carros = [
+    {
+        id: 1,
+        modelo: 'Supra',
+        ano: 1998,
+        marca: 'Toyota',
+        imagem: './img/supra.jpg',
+    },
+    {
+        id: 2,
+        modelo: 'Skyline',
+        ano: 1997,
+        marca: 'Nissan',
+        imagem: './img/skyline.webp'
+    },
+    {
+        id: 3,
+        modelo: 'RX7',
+        ano: 1999,
+        marca: 'Mazda',
+        imagem: './img/rx7.jpg'
+    },
+    {
+        id: 4,
+        modelo: 'Impreza',
+        ano: 1996,
+        marca: 'Subaru',
+        imagem: './img/impreza.jpg'
+    },
+    {
+        id: 5,
+        modelo: 'Lancer',
+        ano: 2008,
+        marca: 'Mitsubish',
+        imagem: './img/lancer.webp'
+    },
+    {
+        id: 6,
+        modelo: 'NSX',
+        ano: 1991,
+        marca: 'Acura',
+        imagem: './img/nsx.jpg',
+    },
+    {
+        id: 7,
+        modelo: 'Civic',
+        ano: 1998,
+        marca: 'Honda',
+        imagem: './img/civic.jpg',
+    }
+];
